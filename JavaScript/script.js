85c11a829e895e70cb224a4e2071e1d629c91a5d
Viky <vikraam17@gmail.com>
2025-06-06 16:40:31 +0530

    javascript completed

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+function sum(){
+    console.log(a+b) 
+}
+var sum =(a,b)=>{
+    return a+b;
+}
+console.log(sum(10,20))
+
+var arr = [10, 20, 30, 40];
+var arr2 = [...arr, 50, 60, 70];
+console.log(arr)
+console.log(arr2)
+
+var marks = [90, 100, 89, 78, 99];
+var [m1, m2, m3, m4, m5] = marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+var a=10
+var b=20
+var sum = a+b;
+console.log(a,"+", b, "=",sum)
+console.log(`${a}+${b}=${sum}`)
+
+var choice = ['rock','paper','scisser']
+var computerChoice = choice[Math.floor(Math.random()*3)]
+console.log(computerChoice)
